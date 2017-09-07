1f7b996af804d969c25184f28cfced18549453da
RettBehrens <everettbehrens@gmail.com>
2017-09-07 07:55:32 -0600

    update working, albeit not how it probably should in the long run

---
+++ src/models/Todo.js
@@ -0,0 +1,40 @@
+// src/models/Todo.js
+import $ from 'jquery'
+
+class TodoModel {
+  static all(){
+    let request = $.ajax({
+      url: "https://super-crud.herokuapp.com/todos",
+      method: 'GET'
+    })
+    return request
+  }
+  static create(todo) {
+	  let request = $.ajax({
+	    url: "https://super-crud.herokuapp.com/todos",
+	    method: 'POST',
+	    data: todo
+	  })
+	  return request
+	}
+  static update(todo){
+    let request = $.ajax({
+      url: "https://super-crud.herokuapp.com/todos/"+todo._id,
+      method: 'PUT',
+      data: {
+        _id: todo._id,
+        body: todo.body
+      }
+    })
+    return request
+  }
+  static delete(todo){
+    let request = $.ajax({
+      url: "https://super-crud.herokuapp.com/todos/"+todo._id,
+      method: 'DELETE'
+    })
+    return request
+  }
+}
+
+export default TodoModel
